0fa94a09a210754d6cee09def347638af74990bf
dylaninsi7 <dylaninsi7@gmail.com>
2024-04-16 19:16:00 -0500

    Add project files.

---
+++ Database_Project_1.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from tkinter import simpledialog, messagebox
+import mysql.connector
+
+password = None
+
+def get_password():
+    global password
+    if password is None:
+        password = simpledialog.askstring("Password", "Enter database password:", show='*')
+    return password
+
+def connect_to_database():
+    try:
+        global password
+        if password is None:
+            password = get_password()
+
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=password,
+            database="student_grade"
+        )
+        return connection
+    except mysql.connector.Error as error:
+        messagebox.showerror("Error", f"Failed to connect to database: {error}")
+        return None
+
